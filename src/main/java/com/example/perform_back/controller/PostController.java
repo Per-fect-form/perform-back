e7186117e1faa0f91831577c47ae4586c8fc05a5
yeilkk <yeil99@naver.com>
2024-07-26 12:54:18 +0900

    feat: swagger  추가

--- src/main/java/com/example/perform_back/controller/PostController.java
+++ src/main/java/com/example/perform_back/controller/PostController.java
@@ -4,6 +4,7 @@ import com.example.perform_back.dto.AttachmentsDto;
 import com.example.perform_back.dto.PostDto;
 import com.example.perform_back.entity.Post;
 import com.example.perform_back.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,26 +24,31 @@ public class PostController {
         this.postService = postService;
     }
 
+    @Operation(summary = "전체 게시글 조회")
     @GetMapping
     public List<Post> getAllPosts(){
         return this.postService.findAll();
     }
 
+    @Operation(summary = "게시글 업로드")
     @PostMapping("/upload")
     public Post createPost(@RequestPart("post") PostDto post, @RequestPart(value = "files", required = false) MultipartFile[] files) {
         return this.postService.save(post, files);
     }
 
+    @Operation(summary = "특정 게시글 조회")
     @GetMapping("/{id}")
     public Post getPost(@PathVariable Long id) {
         return this.postService.findById(id);
     }
 
+    @Operation(summary = "특정 게시글 삭제")
     @DeleteMapping("/{id}")
     public void deletePostById(@PathVariable Long id) {
         this.postService.deleteById(id);
     }
 
+    @Operation(summary = "특정 게시글 수정")
     @PutMapping("/{id}")
     public Post updatePostById(@PathVariable Long id, @RequestPart("post") PostDto postDto,
                                @RequestPart(value = "attachments", required = false) AttachmentsDto attachmentsDto,
